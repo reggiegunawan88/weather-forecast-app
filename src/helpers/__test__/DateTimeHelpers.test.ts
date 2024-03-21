c294abc786f748cb9765e6c9ef8c10e041f551d2
reggiegunawan <reggiegunawan88@gmail.com>
2024-03-21 19:40:13 +0700

    test: update helper test file

--- src/helpers/__test__/DateTimeHelpers.test.ts
+++ src/helpers/__test__/DateTimeHelpers.test.ts
@@ -11,18 +11,47 @@ describe('DateTime Helpers function', () => {
   })
 
   test('Test convertToFullDate() output if shorthand argument is empty', () => {
-    expect(DateTimeHelpers.convertToFullDate({ unix: mockUnixTimestamp })).toEqual('Monday, March 22, 2021')
+    const result = DateTimeHelpers.convertToFullDate({ unix: mockUnixTimestamp })
+
+    // 1. Make sure result type is string
+    expect(typeof result).toBe('string')
+
+    // 2. Define expected parts of the date string
+    const expectedDayOfWeek = 'Monday'
+    const expectedMonth = 'March'
+    const expectedDay = '22'
+    const expectedYear = '2021'
+
+    // 3. Make sure that result matches the expected format
+    expect(result).toMatch(new RegExp(`${expectedDayOfWeek}, ${expectedMonth} ${expectedDay}, ${expectedYear}`))
   })
 
   test('Test convertTo24HrsFormat() output', () => {
-    expect(DateTimeHelpers.convertTo24HrsFormat({ unix: mockUnixTimestamp })).toEqual('22')
+    const result = DateTimeHelpers.convertTo24HrsFormat({ unix: mockUnixTimestamp })
+
+    // 1. Define expected time format
+    const expectedFormat = /^(0[0-9]|1[0-9]|2[0-3])$/
+
+    // 2. Make sure result type is string
+    expect(typeof result).toBe('string')
+
+    // 3. Make sure result matches the expected format
+    expect(result).toMatch(expectedFormat)
   })
 
   test('Test isPresentTime() output', () => {
     expect(DateTimeHelpers.isPresentTime({ unix: mockUnixTimestamp })).toBeFalsy()
   })
 
-  test('Test isToday() output', () => {
-    expect(DateTimeHelpers.isToday({ unix: mockUnixTimestamp })).toBeFalsy()
+  test('isToday() should return true if the date is today', () => {
+    // 1. Get today Unix timestamp
+    const todayTimestamp = Math.floor(Date.now() / 1000)
+
+    // 2. Make sure function is returning true
+    expect(DateTimeHelpers.isToday({ unix: todayTimestamp })).toBeTruthy()
+  })
+
+  test('isToday() should return false if the date is not today', () => {
+    expect(DateTimeHelpers.isToday({ unix: 1616425200 })).toBeFalsy()
   })
 })
