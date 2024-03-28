833d3295daab63b7c1bdd43ce5a59633882af7eb
reggiegunawan <reggiegunawan88@gmail.com>
2024-03-28 12:06:38 +0700

    fix(ui): update base bg color and empty state layout

--- src/components/common/EmptyState/index.tsx
+++ src/components/common/EmptyState/index.tsx
@@ -4,9 +4,9 @@ interface Props {
 
 function EmptyState({ description }: Props) {
   return (
-    <main className="flex flex-1 justify-center p-4 text-center items-center">
+    <div className="flex h-full justify-center p-4 text-center items-center">
       <p className="text-xl font-semibold capitalize">{description}</p>
-    </main>
+    </div>
   )
 }
 
